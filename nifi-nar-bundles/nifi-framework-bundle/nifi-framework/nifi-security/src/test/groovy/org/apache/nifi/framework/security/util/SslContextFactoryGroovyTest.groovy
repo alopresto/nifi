d822a736881789f37882fb19c375a34fe1653d57
Andy LoPresto <alopresto.apache@gmail.com>
2016-03-31 19:58:27 -0700

    NIFI-1478 Added passing baseline regression test for current NiFi 0.6.0 default SSLContext parameters (protocols & cipher suites).

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-security/src/test/groovy/org/apache/nifi/framework/security/util/SslContextFactoryGroovyTest.groovy
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-security/src/test/groovy/org/apache/nifi/framework/security/util/SslContextFactoryGroovyTest.groovy
@@ -29,6 +29,7 @@ import org.junit.runners.JUnit4
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import javax.net.ssl.SSLContext
 import java.security.Security
 
 import static org.apache.nifi.util.NiFiProperties.SECURITY_KEYSTORE
@@ -38,11 +39,14 @@ import static org.apache.nifi.util.NiFiProperties.SECURITY_NEED_CLIENT_AUTH
 import static org.apache.nifi.util.NiFiProperties.SECURITY_TRUSTSTORE
 import static org.apache.nifi.util.NiFiProperties.SECURITY_TRUSTSTORE_PASSWD
 import static org.apache.nifi.util.NiFiProperties.SECURITY_TRUSTSTORE_TYPE
+import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.when
 
 @RunWith(JUnit4.class)
 class SslContextFactoryGroovyTest extends GroovyTestCase {
     private static final Logger logger = LoggerFactory.getLogger(SslContextFactoryGroovyTest.class)
 
+    private static def MOZILLA_CONFIGURATIONS
     private static Map<String, List<String>> MOZILLA_CIPHER_SUITES
     private static List<String> CUSTOM_NIFI_CIPHER_SUITES
 
@@ -59,6 +63,95 @@ class SslContextFactoryGroovyTest extends GroovyTestCase {
     private static final String TRUSTSTORE_PASSWORD = "localtest"
 
     private SslContextFactory sslContextFactory
+    private static final Map LEGACY_NIFI_060_CONFIGURATION = [
+            tls_versions: ["SSLv2Hello", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"],
+            ciphersuites: legacyNiFi060CipherSuites()
+    ]
+
+    private static List<String> legacyNiFi060CipherSuites() {
+        ["TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384",
+         "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
+         "TLS_RSA_WITH_AES_256_CBC_SHA256",
+         "TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA384",
+         "TLS_ECDH_RSA_WITH_AES_256_CBC_SHA384",
+         "TLS_DHE_RSA_WITH_AES_256_CBC_SHA256",
+         "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256",
+         "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
+         "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+         "TLS_RSA_WITH_AES_256_CBC_SHA",
+         "TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA",
+         "TLS_ECDH_RSA_WITH_AES_256_CBC_SHA",
+         "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
+         "TLS_DHE_DSS_WITH_AES_256_CBC_SHA",
+         "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
+         "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
+         "TLS_RSA_WITH_AES_128_CBC_SHA256",
+         "TLS_ECDH_ECDSA_WITH_AES_128_CBC_SHA256",
+         "TLS_ECDH_RSA_WITH_AES_128_CBC_SHA256",
+         "TLS_DHE_RSA_WITH_AES_128_CBC_SHA256",
+         "TLS_DHE_DSS_WITH_AES_128_CBC_SHA256",
+         "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
+         "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+         "TLS_RSA_WITH_AES_128_CBC_SHA",
+         "TLS_ECDH_ECDSA_WITH_AES_128_CBC_SHA",
+         "TLS_ECDH_RSA_WITH_AES_128_CBC_SHA",
+         "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
+         "TLS_DHE_DSS_WITH_AES_128_CBC_SHA",
+         "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+         "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+         "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+         "TLS_RSA_WITH_AES_256_GCM_SHA384",
+         "TLS_ECDH_ECDSA_WITH_AES_256_GCM_SHA384",
+         "TLS_ECDH_RSA_WITH_AES_256_GCM_SHA384",
+         "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
+         "TLS_DHE_DSS_WITH_AES_256_GCM_SHA384",
+         "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+         "TLS_RSA_WITH_AES_128_GCM_SHA256",
+         "TLS_ECDH_ECDSA_WITH_AES_128_GCM_SHA256",
+         "TLS_ECDH_RSA_WITH_AES_128_GCM_SHA256",
+         "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256",
+         "TLS_DHE_DSS_WITH_AES_128_GCM_SHA256",
+         "TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA",
+         "TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA",
+         "SSL_RSA_WITH_3DES_EDE_CBC_SHA",
+         "TLS_ECDH_ECDSA_WITH_3DES_EDE_CBC_SHA",
+         "TLS_ECDH_RSA_WITH_3DES_EDE_CBC_SHA",
+         "SSL_DHE_RSA_WITH_3DES_EDE_CBC_SHA",
+         "SSL_DHE_DSS_WITH_3DES_EDE_CBC_SHA",
+         "TLS_EMPTY_RENEGOTIATION_INFO_SCSV",
+         "TLS_DH_anon_WITH_AES_256_GCM_SHA384",
+         "TLS_DH_anon_WITH_AES_128_GCM_SHA256",
+         "TLS_DH_anon_WITH_AES_256_CBC_SHA256",
+         "TLS_ECDH_anon_WITH_AES_256_CBC_SHA",
+         "TLS_DH_anon_WITH_AES_256_CBC_SHA",
+         "TLS_DH_anon_WITH_AES_128_CBC_SHA256",
+         "TLS_ECDH_anon_WITH_AES_128_CBC_SHA",
+         "TLS_DH_anon_WITH_AES_128_CBC_SHA",
+         "TLS_ECDH_anon_WITH_3DES_EDE_CBC_SHA",
+         "SSL_DH_anon_WITH_3DES_EDE_CBC_SHA",
+         "SSL_RSA_WITH_DES_CBC_SHA",
+         "SSL_DHE_RSA_WITH_DES_CBC_SHA",
+         "SSL_DHE_DSS_WITH_DES_CBC_SHA",
+         "SSL_DH_anon_WITH_DES_CBC_SHA",
+         "SSL_RSA_EXPORT_WITH_DES40_CBC_SHA",
+         "SSL_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA",
+         "SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA",
+         "SSL_DH_anon_EXPORT_WITH_DES40_CBC_SHA",
+         "TLS_RSA_WITH_NULL_SHA256",
+         "TLS_ECDHE_ECDSA_WITH_NULL_SHA",
+         "TLS_ECDHE_RSA_WITH_NULL_SHA",
+         "SSL_RSA_WITH_NULL_SHA",
+         "TLS_ECDH_ECDSA_WITH_NULL_SHA",
+         "TLS_ECDH_RSA_WITH_NULL_SHA",
+         "TLS_ECDH_anon_WITH_NULL_SHA",
+         "SSL_RSA_WITH_NULL_MD5",
+         "TLS_KRB5_WITH_3DES_EDE_CBC_SHA",
+         "TLS_KRB5_WITH_3DES_EDE_CBC_MD5",
+         "TLS_KRB5_WITH_DES_CBC_SHA",
+         "TLS_KRB5_WITH_DES_CBC_MD5",
+         "TLS_KRB5_EXPORT_WITH_DES_CBC_40_SHA",
+         "TLS_KRB5_EXPORT_WITH_DES_CBC_40_MD5"]
+    }
 
     @BeforeClass
     static void setUpOnce() {
@@ -68,7 +161,8 @@ class SslContextFactoryGroovyTest extends GroovyTestCase {
             logger.info("[${name?.toUpperCase()}] ${(args as List).join(" ")}")
         }
 
-//        retrieveTLSConfigurationsFromMozilla()
+        MOZILLA_CONFIGURATIONS = retrieveTLSConfigurationsFromMozilla().configurations
+        MOZILLA_CIPHER_SUITES = parseCipherSuitesFromConfigurations(MOZILLA_CONFIGURATIONS)
         setUpProperties()
     }
 
@@ -80,74 +174,74 @@ class SslContextFactoryGroovyTest extends GroovyTestCase {
                 (SECURITY_KEYSTORE)        : keystoreFile.getAbsolutePath(),
                 (SECURITY_KEYSTORE_TYPE)   : KeystoreType.JKS as String,
                 (SECURITY_KEYSTORE_PASSWD) : KEYSTORE_PASSWORD,
-                (SECURITY_NEED_CLIENT_AUTH): false
+                (SECURITY_NEED_CLIENT_AUTH): "false"
         ]
 
         DEFAULT_PROPS = mockProperties(propertiesMap)
 
-        MUTUAL_AUTH_PROPS = propertiesMap + [
+        MUTUAL_AUTH_PROPS = mockProperties(propertiesMap + [
                 (SECURITY_TRUSTSTORE)       : truststoreFile.getAbsolutePath(),
                 (SECURITY_TRUSTSTORE_TYPE)  : KeystoreType.JKS as String,
                 (SECURITY_TRUSTSTORE_PASSWD): TRUSTSTORE_PASSWORD,
-                (SECURITY_NEED_CLIENT_AUTH) : true
-        ] as NiFiProperties
+                (SECURITY_NEED_CLIENT_AUTH) : "true"
+        ])
     }
 
     private static NiFiProperties mockProperties(Map props) {
-        NiFiProperties nfp = new NiFiProperties(props)
-        nfp.getMetaClass().getProperty = { String propName ->
-            if (props.containsKey(propName)) {
-                props.get(propName)
-            } else {
-                ""
-            }
-        }
-        nfp.getMetaClass().getNeedClientAuth = { ->
-            props.get(SECURITY_NEED_CLIENT_AUTH) ?: "true"
+        def nfp = mock(NiFiProperties.class) as NiFiProperties
+        props.each { String k, String v ->
+            when(nfp.getProperty(k)).thenReturn(v)
         }
 
-//        nfp.getMetaClass().invokeMethod = { String methodName, def args ->
-//            if (methodName == "getMetaClass") {
-//                return getMetaClass()
-//            }
-//            logger.invoked("Tried to call NiFiProperties.${methodName}(${args})")
-//            def m = delegate.getMetaClass().getMetaMethod(methodName, *args)
-//            if (m) {
-////                delegate."$methodName"(*args)
-////                logger.debug("Type of delegate: ${delegate.getClass().getCanonicalName()}")
-//                m.invoke(delegate , *args)
-//            } else {
-//                delegate.getMetaClass().invokeMissingMethod(delegate, methodName, args)
-//            }
-////            m ? m.invoke(delegate, *args) : delegate.getMetaClass().invokeMissingMethod(delegate, methodName, args)
-//        }
-
-        nfp.getMetaClass().methodMissing = { String method, def args ->
-            logger.methodMissing("Tried to call missing NiFiProperties.${method}(${args})")
-            return null
-        }
-        nfp.getMetaClass().toString = { ->
-            delegate.dump()
-        }
+        when(nfp.getNeedClientAuth()).thenReturn(props.get(SECURITY_NEED_CLIENT_AUTH) != "false")
 
         nfp
     }
 
-    private static void retrieveTLSConfigurationsFromMozilla() {
-        MOZILLA_CIPHER_SUITES = [:]
-
+    private static def retrieveTLSConfigurationsFromMozilla() {
         def mozillaTlsJson = MOZILLA_CURRENT_TLS_URL.toURL().text
         logger.info("Retrieved from Mozilla: ${mozillaTlsJson}")
         JsonSlurper slurper = new JsonSlurper()
-        def json = slurper.parseText(mozillaTlsJson)
-        assert json.configurations.modern
-        MOZILLA_CIPHER_SUITES[MZ_MODERN] = json.configurations.modern.ciphersuites
+        slurper.parseText(mozillaTlsJson)
+    }
 
-        assert json.configurations.intermediate
-        MOZILLA_CIPHER_SUITES[MZ_INTERMEDIATE] = json.configurations.intermediate.ciphersuites
+    public static void parseCipherSuitesFromConfigurations(jsonConfigurations) {
+        def configurations = [:]
 
-        assert json.configurations.old
-        MOZILLA_CIPHER_SUITES[MZ_OLD] = json.configurations.old.ciphersuites
+        assert jsonConfigurations.modern
+        configurations[MZ_MODERN] = jsonConfigurations.modern.ciphersuites
+
+        assert jsonConfigurations.intermediate
+        configurations[MZ_INTERMEDIATE] = jsonConfigurations.intermediate.ciphersuites
+
+        assert jsonConfigurations.old
+        configurations[MZ_OLD] = jsonConfigurations.old.ciphersuites
+
+        configurations
+    }
+
+    private static boolean sslContextMeetsConfiguration(SSLContext context, def configuration) {
+        // Ensure an SSLContext exists
+        def params = context?.supportedSSLParameters
+        assert params
+
+        // Parse the supported protocols and cipher suites from the SSLContext
+        List<String> protocols = params.protocols as List
+        logger.context("Protocols: ${protocols}")
+        List<String> cipherSuites = params.cipherSuites as List
+        logger.context("Cipher suites: ${cipherSuites}")
+
+
+        // Parse the expected protocols and cipher suites from the configuration
+        List<String> configProtocols = configuration.tls_versions as List
+        logger.config("Protocols: ${configProtocols}")
+        List<String> configCipherSuites = configuration.ciphersuites as List
+        logger.config("Cipher suites: ${configCipherSuites}")
+
+        assert protocols as Set == configProtocols as Set, "Protocols do not match"
+        assert cipherSuites as Set == configuration.ciphersuites as Set, "Cipher suites do not match"
+
+        true
     }
 
     @Before
@@ -162,7 +256,20 @@ class SslContextFactoryGroovyTest extends GroovyTestCase {
     }
 
     @Test
-    void testDefaultCreateSslContextCipherSuitesShouldMeetIntermediateThreshold() {
+    void testDefaultCreateSslContextCipherSuitesShouldMeetLegacyNiFiConfiguration() {
+        // Arrange
+        sslContextFactory = new SslContextFactory()
+
+        // Act
+        def sslContext = sslContextFactory.createSslContext(DEFAULT_PROPS)
+        logger.info("SSL Context: ${sslContext?.dump()?.split(/, /)?.join("\n")}")
+
+        // Assert
+        assert sslContextMeetsConfiguration(sslContext, LEGACY_NIFI_060_CONFIGURATION)
+    }
+
+    @Test
+    void testDefaultCreateSslContextCipherSuitesShouldMeetMozillaIntermediateConfiguration() {
         // Arrange
         sslContextFactory = new SslContextFactory()
         logger.settings(DEFAULT_PROPS)
@@ -174,10 +281,9 @@ class SslContextFactoryGroovyTest extends GroovyTestCase {
 
         // Act
         def sslContext = sslContextFactory.createSslContext(DEFAULT_PROPS)
-        logger.info("SSL Context: ${sslContext?.dump()}")
+        logger.info("SSL Context: ${sslContext?.dump()?.split(/, /)?.join("\n")}")
 
         // Assert
-        assert sslContext
-
+        assert sslContextMeetsConfiguration(sslContext, MOZILLA_CONFIGURATIONS.intermediate)
     }
 }
