86a12d6def01c027c326de91adfc4433d59673f2
Andy LoPresto <alopresto@apache.org>
2016-11-22 01:03:23 -0800

    NIFI-3024 Added unit tests for flow.xml.gz argument parsing.

--- nifi-toolkit/nifi-toolkit-encrypt-config/src/test/groovy/org/apache/nifi/properties/ConfigEncryptionToolTest.groovy
+++ nifi-toolkit/nifi-toolkit-encrypt-config/src/test/groovy/org/apache/nifi/properties/ConfigEncryptionToolTest.groovy
@@ -2630,10 +2630,57 @@ class ConfigEncryptionToolTest extends GroovyTestCase {
         // Assertions defined above
     }
 
-    // TODO: -x should not encrypt nifi.properties or lip.xml
-    // TODO: -f without -n should fail
+    @Test
+    void testParseShouldIgnoreFilesIfOverrideFlagPresent() {
+        // Arrange
+        String niFiPropertiesPath = "conf/nifi.properties"
+        String flowXmlPath = "conf/flow.xml.gz"
+        ConfigEncryptionTool tool = new ConfigEncryptionTool()
+
+        // Act
+        tool.parse("-n ${niFiPropertiesPath} -f ${flowXmlPath} -x".split(" ") as String[])
+
+        // Assert
+        assert !tool.handlingNiFiProperties
+        assert !tool.handlingLoginIdentityProviders
+        assert tool.handlingFlowXml
+    }
+
+    @Test
+    void testParseShouldWarnIfFlowXmlWillBeOverwritten() {
+        // Arrange
+        String niFiPropertiesPath = "conf/nifi.properties"
+        String flowXmlPath = "conf/flow.xml.gz"
+        ConfigEncryptionTool tool = new ConfigEncryptionTool()
+
+        // Act
+        tool.parse("-n ${niFiPropertiesPath} -f ${flowXmlPath}".split(" ") as String[])
+        logger.info("Parsed nifi.properties location: ${tool.niFiPropertiesPath}")
+        logger.info("Parsed flow.xml.gz location: ${tool.flowXmlPath}")
+        logger.info("Parsed output flow.xml.gz location: ${tool.outputFlowXmlPath}")
+
+        // Assert
+        assert !TestAppender.events.isEmpty()
+        assert TestAppender.events.any {it.message =~ "The source flow.xml.gz and destination flow.xml.gz are identical \\[.*\\] so the original will be overwritten" }
+    }
+
+    @Test
+    void testParseShouldFailOnFlowWithoutNiFiProperties() {
+        // Arrange
+        String flowXmlPath = "conf/flow.xml.gz"
+        ConfigEncryptionTool tool = new ConfigEncryptionTool()
+
+        // Act
+        def msg = shouldFail(CommandLineParseException) {
+            tool.parse("-f ${flowXmlPath} -x".split(" ") as String[])
+        }
+        logger.expected(msg)
+
+        // Assert
+        assert msg == "In order to migrate a flow.xml.gz, a nifi.properties file must also be specified via '-n'/'--niFiProperties'." as String
+    }
+
     // TODO: All combo scenarios
-    // TODO: -n without -x should encrypt nifi.properties (probably already covered)
 
     @Test
     void testDecryptFlowXmlContentShouldVerifyPattern() {
@@ -2947,48 +2994,7 @@ class ConfigEncryptionToolTest extends GroovyTestCase {
         assert tool.loadFlowXml() == xmlContent
     }
 
-    // TODO: Test cipher text extraction and replacement in XML (no attribute update necessary)
     // TODO: Test with 128/256-bit available
-    // Not a complete flow but sufficient to test XML extraction, crypto, and replacement -- the encrypted value is "thisIsABadPassword"
-//    String xml = """
-//<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-//<flowController encoding-version="1.0">
-//  <rootGroup>
-//    <id>fcf146b2-0157-1000-7850-7adf1d31e3fa</id>
-//    <name>NiFi Flow</name>
-//    <position x="0.0" y="0.0"/>
-//    <comment/>
-//    <processGroup>
-//      <id>8a61ec1d-0158-1000-3a1a-12c54fe77838</id>
-//      <name>EncryptedProperties Example</name>
-//      <position x="1119.0" y="295.0"/>
-//      <comment/>
-//      <processor>
-//        <id>8a621f0b-0158-1000-b5c2-92a09a124501</id>
-//        <name>Encrypt</name>
-//        <position x="626.0" y="237.0"/>
-//        <styles/>
-//        <comment/>
-//        <class>org.apache.nifi.processors.standard.EncryptContent</class>
-//        <property>
-//          <name>Password</name>
-//          <value>enc{2032416987A00D9FCD757528D7AE609D7E793CA5F956641DB53E14CDB9BFCD4037B73AC705CD3F5C1C1BDE18B8D7B281}</value>
-//        </property>
-//      </processor>
-//    </processGroup>
-//  </rootGroup>
-//</flowController>
-//"""
-
-    // Not a complete flow but sufficient to test XML extraction, crypto, and replacement -- the encrypted value is "thisIsABadPassword"
-//    String xml = """
-//<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-//        <property>
-//          <name>Password</name>
-//          <value>enc{2032416987A00D9FCD757528D7AE609D7E793CA5F956641DB53E14CDB9BFCD4037B73AC705CD3F5C1C1BDE18B8D7B281}</value>
-//        </property>
-//"""
-
 }
 
 public class TestAppender extends AppenderSkeleton {
