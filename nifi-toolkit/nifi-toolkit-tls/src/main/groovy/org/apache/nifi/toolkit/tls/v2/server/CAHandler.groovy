d76537de74104e435d3a868039a8735201cc3852
Andy LoPresto <alopresto@apache.org>
2018-09-25 10:49:35 -0700

    NIFI-5462 Handled invalid HMAC response. Refactored CAHandler to component methods. Added unit tests.

--- nifi-toolkit/nifi-toolkit-tls/src/main/groovy/org/apache/nifi/toolkit/tls/v2/server/CAHandler.groovy
+++ nifi-toolkit/nifi-toolkit-tls/src/main/groovy/org/apache/nifi/toolkit/tls/v2/server/CAHandler.groovy
@@ -6,6 +6,7 @@ import org.apache.nifi.toolkit.tls.v2.ca.CAService
 import org.apache.nifi.toolkit.tls.v2.util.TlsToolkitUtil
 import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequest
 import org.eclipse.jetty.server.Request
+import org.eclipse.jetty.server.Response
 import org.eclipse.jetty.server.handler.AbstractHandler
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -17,6 +18,9 @@ import java.security.GeneralSecurityException
 import java.security.cert.Certificate
 import java.security.cert.X509Certificate
 
+/**
+ * Jetty handler to perform the CSR signing.
+ */
 class CAHandler extends AbstractHandler {
     private static final Logger logger = LoggerFactory.getLogger(CAHandler.class)
 
@@ -27,59 +31,116 @@ class CAHandler extends AbstractHandler {
         logger.info("Created a CA handler with ${cas}")
     }
 
+    /**
+     * Accepts a JSON payload in the request body containing a PEM-encoded (Base64) CSR and the generated HMAC/ SHA-256(token, CSR public key) and sends a response containing the PEM-encoded certificate chain, including the CA cert and signed leaf cert. If an exception occurs, sends an error response containing a JSON payload with descriptive errors.
+     *
+     * @param s the target
+     * @param request the base request
+     * @param httpServletRequest the request containing the payload
+     * @param httpServletResponse the response containing the payload
+     * @throws IOException
+     * @throws ServletException
+     */
     @Override
     void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
         logger.info("Received CSR signing request ${s} from ${httpServletRequest.remoteAddr}")
+        String csrDn = "unable to parse CSR subject"
 
-        // Parse the incoming request and extract the body (JSON containing Base64-encoded CSR)
+        try {
+            // Parse the incoming request and extract the body (JSON containing Base64-encoded CSR)
+            Map parsedJson = extractRequestJson(httpServletRequest)
+
+            String hmac = parsedJson.hmac
+            logger.debug("Extracted HMAC from request: ${hmac}")
+
+            JcaPKCS10CertificationRequest csr = decodeCsrFromJson(parsedJson)
+            csrDn = csr.subject
+
+            // Try to sign the CSR
+            String chainBase64 = signAndEncodeCertificateChain(csr, hmac)
+
+            // Form the response
+            String responseJson = formSuccessResponseJson(csrDn, chainBase64)
+
+            // Send the response
+            httpServletResponse.setContentType("application/json")
+            httpServletResponse.writer.write(responseJson)
+            httpServletResponse.setStatus(Response.SC_OK)
+        } catch (GeneralSecurityException e) {
+            logger.error("Encountered a problem signing the CSR -- ${e.message}")
+
+            // Form the response
+            String responseJson = formErrorResponseJson(csrDn, e.message)
+
+            // Send the response
+            httpServletResponse.setContentType("application/json")
+            httpServletResponse.writer.write(responseJson)
+            httpServletResponse.setStatus(Response.SC_FORBIDDEN)
+        }
+    }
+
+    protected String formSuccessResponseJson(String csrDn, String chainBase64) {
+        // Form response map
+        Map responseMap = [
+                message         : "Successfully signed certificate for ${csrDn} with CA ${getCACertName()}",
+                certificateChain: chainBase64
+        ]
+
+        // Form response JSON
+        String responseJson = JsonOutput.toJson(responseMap)
+        logger.debug("Formed response JSON: ${responseJson}")
+        responseJson
+    }
+
+    protected String formErrorResponseJson(String csrDn, String errorMessage) {
+        // Form response map
+        Map responseMap = [
+                message     : "Unable to sign ${csrDn} with CA ${getCACertName()}",
+                errorMessage: errorMessage
+        ]
+
+        // Form response JSON
+        String responseJson = JsonOutput.toJson(responseMap)
+        logger.debug("Formed response JSON: ${responseJson}")
+        responseJson
+    }
+
+    private String getCACertName() {
+        caService.caCert.subjectX500Principal.name
+    }
+
+    protected String signAndEncodeCertificateChain(JcaPKCS10CertificationRequest csr, String hmac) {
+        X509Certificate signedCertificate = caService.signCSR(csr, hmac)
+        logger.info("Signed the certificate for ${csr.subject}")
+
+        // Concat the signed certificate and the CA certificate
+        List<Certificate> certificateChain = [caService.caCert, signedCertificate]
+        logger.info("Formed the certificate chain [${certificateChain*.subjectX500Principal.name.join(" -> ")}] to return")
+
+        // Base64 encode the certificate chain
+        String chainBase64 = certificateChain.collect { Certificate cert ->
+            TlsToolkitUtil.pemEncode(cert)
+        }.join()
+        logger.info("Encoded the certificate chain to PEM: ${chainBase64}")
+        chainBase64
+    }
+
+    protected static JcaPKCS10CertificationRequest decodeCsrFromJson(Map parsedJson) {
+        String csrBase64 = parsedJson.csr
+        logger.debug("Extracted CSR in Base64 encoding: ${csrBase64}")
+
+        // Decode the CSR
+        JcaPKCS10CertificationRequest csr = TlsToolkitUtil.decodeCsr(csrBase64)
+        logger.debug("Decoded PEM CSR for DN: ${csr.subject}")
+        csr
+    }
+
+    protected static Map extractRequestJson(HttpServletRequest httpServletRequest) {
         String jsonContents = httpServletRequest.reader.readLines().join(System.lineSeparator())
         logger.debug("Read JSON contents: ${jsonContents}")
 
         Map parsedJson = new JsonSlurper().parseText(jsonContents) as Map
         logger.debug("Parsed request JSON to keys: ${parsedJson.keySet()}")
-
-        String hmac = parsedJson.hmac
-        logger.debug("Extracted HMAC from request: ${hmac}")
-        String csrBase64 = parsedJson.csr
-        logger.debug("Extracted CSR in Base64 encoding: ${csrBase64}")
-
-        // Decode the CSR
-//        byte[] csrBytes = Base64.decoder.decode(csrBase64)
-        JcaPKCS10CertificationRequest csr = TlsToolkitUtil.decodeCsr(csrBase64)
-        logger.debug("Decoded PEM CSR for DN: ${csr.subject}")
-
-        // Try to sign the CSR
-        try {
-            X509Certificate signedCertificate = caService.signCSR(csr, hmac)
-            logger.info("Signed the certificate for ${csr.subject}")
-
-            // Concat the signed certificate and the CA certificate
-            List<Certificate> certificateChain = [caService.caCert, signedCertificate]
-            logger.info("Formed the certificate chain [${certificateChain*.subjectX500Principal.name.join(" -> ")}] to return")
-
-            // Base64 encode the certificate chain
-            String chainBase64 = certificateChain.collect { Certificate cert ->
-                TlsToolkitUtil.pemEncode(cert)
-            }.join()
-            logger.info("Encoded the certificate chain to PEM: ${chainBase64}")
-
-            // Form response map
-            Map responseMap = [message: "Successfully signed certificate for ${csr.subject} with CA ${caService.caCert.subjectX500Principal.name}",
-                               certificateChain: chainBase64]
-
-            // Form response JSON
-            String responseJson = JsonOutput.toJson(responseMap)
-            logger.debug("Formed response JSON: ${responseJson}")
-
-            // Send as response
-            httpServletResponse.writer.write(responseJson)
-        } catch (GeneralSecurityException e) {
-            logger.error("Encountered a problem signing the CSR -- the provided HMAC is invalid")
-            throw new ServletException(e) // ?
-        }
-
-        // Send the response
-
-        // TODO: Send 50x response
+        parsedJson
     }
 }
